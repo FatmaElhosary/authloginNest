7e8e62869a685ec61e5d2b25b00b360d9250c107
Fatma <engfatmafathy@gmail.com>
2023-01-11 11:43:17 +0200

    save

--- src/models/user.schema.ts
+++ src/models/user.schema.ts
@@ -1,14 +1,20 @@
 
+
 import * as mongoose from 'mongoose';
 import * as bcrypt from 'bcrypt';
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 
-export const UserSchema=new mongoose.Schema({
-    email:{type:String,unique:true,required:true},
-    password:{type:String,required:true},
+@Schema()
+export class User{
 
-});
+    @Prop()
+    email:string;
+    @Prop()
+    password:string;
+  }
+  export const UserSchema = SchemaFactory.createForClass(User);
 ///mongoose.HookNextFunction
-UserSchema.pre('save', async function(next: any) {
+ UserSchema.pre('save', async function(next: any) {
     try {
       if (!this.isModified('password')) {
         return next();
@@ -19,4 +25,4 @@ UserSchema.pre('save', async function(next: any) {
     } catch (err) {
       return next(err);
     }
-  });
+  }); 
--- src/user/DTO/register.dto.ts
+++ src/user/DTO/register.dto.ts
@@ -1,4 +1,10 @@
-export interface RegisterDTO {
+
+import { IsEmail, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class RegisterDTO {
+    @IsNotEmpty()
+    @IsEmail()
     email:string;
+    @IsNotEmpty()
     password: string;
   }
